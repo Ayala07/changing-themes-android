0b639f50bd7d0f167ac35ab0ee7f48c374e2e37b
Roberto Ayala <robertube26@gmail.com>
2021-03-01 18:42:43 -0600

    Se añade el bloque de codigo para cambiar de tema

--- app/src/main/java/com/example/cambiosdetemas/MainActivity.kt
+++ app/src/main/java/com/example/cambiosdetemas/MainActivity.kt
@@ -2,10 +2,43 @@ package com.example.cambiosdetemas
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AlertDialog
+import androidx.appcompat.app.AppCompatDelegate
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        btnChangeTheme.setOnClickListener { changeTheme() }
+    }
+
+    private fun changeTheme(){
+        val builder = AlertDialog.Builder(this)
+        builder.setTitle(getString(R.string.btnString))
+        val styles = arrayOf(getString(R.string.rbd1Name),getString(R.string.rbd2Name), getString(R.string.rbd3Name))
+        val checkItem = 0
+        builder.setSingleChoiceItems(styles,checkItem){dialog, which ->
+            when(which){
+                0 ->{
+                    AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
+                    delegate.applyDayNight()
+                    dialog.dismiss()
+                }
+                1 ->{
+                    AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES)
+                    delegate.applyDayNight()
+                    dialog.dismiss()
+                }
+                2 ->{
+                    AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_FOLLOW_SYSTEM)
+                    delegate.applyDayNight()
+                    dialog.dismiss()
+                }
+            }
+            val dialog = builder.create()
+            dialog.show()
+        }
     }
 }
