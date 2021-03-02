6cbd037b8f241953522e8800a43514da5b587574
Roberto Ayala <robertube26@gmail.com>
2021-03-01 18:48:07 -0600

    Se añade el bloque para guardar el estado de tema seleccionado

--- app/src/main/java/com/example/cambiosdetemas/MainActivity.kt
+++ app/src/main/java/com/example/cambiosdetemas/MainActivity.kt
@@ -1,9 +1,11 @@
 package com.example.cambiosdetemas
 
+import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatDelegate
+import androidx.preference.PreferenceManager
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
@@ -23,16 +25,19 @@ class MainActivity : AppCompatActivity() {
             when(which){
                 0 ->{
                     AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
+                    MyPreferences(this).darkMode = 0
                     delegate.applyDayNight()
                     dialog.dismiss()
                 }
                 1 ->{
                     AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES)
+                    MyPreferences(this).darkMode = 1
                     delegate.applyDayNight()
                     dialog.dismiss()
                 }
                 2 ->{
                     AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_FOLLOW_SYSTEM)
+                    MyPreferences(this).darkMode = 2
                     delegate.applyDayNight()
                     dialog.dismiss()
                 }
@@ -40,5 +45,15 @@ class MainActivity : AppCompatActivity() {
             val dialog = builder.create()
             dialog.show()
         }
+    }
+
+    class MyPreferences(context: Context?){
+        companion object{
+            private const val DARK_STATUS = "DARK STATUS"
+        }
+        private val preferences = PreferenceManager.getDefaultSharedPreferences(context)
+
+        var darkMode = preferences.getInt(DARK_STATUS,0)
+        set(value) = preferences.edit().putInt(DARK_STATUS,value).apply()
     }
 }
